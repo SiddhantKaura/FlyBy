e65d9c893a935080e70b39b713c0cbd6b5a6a80d
Siddhant Kaura <siddhantkaura@Siddhants-Air.bbrouter>
2026-09-18 00:41:03 +0530

    feat: Added loation entity and extended it in reviews

--- subgraph-locations/resolvers.js
+++ subgraph-locations/resolvers.js
@@ -7,6 +7,13 @@ const resolvers = {
       return dataSources.locationsAPI.getLocation(id);
     },
   },
+  Location: {
+    __resolveReference: (reference, context) => {
+      const { id } = reference;
+      const { dataSources } = context;
+      return dataSources.locationsAPI.getLocation(id);
+    },
+  },
 };
 
 module.exports = resolvers;
